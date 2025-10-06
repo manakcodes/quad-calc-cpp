04e7a97d1374c8bd622fa32ab69ff59226c2c937
manakcodes <manakcodes.1@gmail.com>
2025-10-06 17:49:25 +0530

    ADDED -> (quadcalc.hpp) :: the master file with all the headers linked into it

---
+++ quadcalc.hpp
@@ -0,0 +1,9 @@
+// ========================================================================== //
+// HEADER FILES SECTION
+// ========================================================================== //
+
+#pragma once
+#include "include/quadratic.hpp"
+#include "src/quadratic_core.hpp"
+#include "src/quadratic_cli.hpp"
+
